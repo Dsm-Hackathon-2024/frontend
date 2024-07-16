fc425f1c29bc83867c5e7e9dddcd3e5abf04c521
정승우 <ssounsowns@gmail.com>
2024-07-17 01:26:14 +0900

    themeProvider 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./style/GlobalStyle";
+import theme from "./style/theme";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>
+);
