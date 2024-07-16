f34b10a0fdcd00ff4eeeb9d31d4e4f842b62bb0e
정승우 <ssounsowns@gmail.com>
2024-07-16 18:59:25 +0900

    GlobalStyle 사용

--- Dsm-Hackathon-2024/src/App.jsx
+++ Dsm-Hackathon-2024/src/App.jsx
@@ -1,7 +1,15 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./routes/Login";
+import GlobalStyle from "./style/GlobalStyle";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle />
+      <Login />
+    </>
+  );
 }
 
 export default App;
