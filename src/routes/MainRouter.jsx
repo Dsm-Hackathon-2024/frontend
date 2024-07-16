1b60dc8d74451ae6b49bb6dadfaf3deeff1d1bad
dutexion <dutexion@gmail.com>
2024-07-17 01:54:43 +0900

    search page routing

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
 import Home from "../pages/Home";
+import Search from "../pages/Search";
 
 function MainRouter() {
   return (
@@ -10,6 +11,7 @@ function MainRouter() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
