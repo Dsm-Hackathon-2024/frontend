763c76fca5b7e3d8e8156a9dce7b8bc47acbd506
정승우 <ssounsowns@gmail.com>
2024-07-16 18:56:20 +0900

    style 파일들 추가
    GlobalStyle, StyledProvider, theme 추가

---
+++ Dsm-Hackathon-2024/src/style/GlobalStyle.jsx
@@ -0,0 +1,67 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 900;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Black.woff')
+      format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 700;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff')
+      format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff')
+      format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 500;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Medium.woff')
+      format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 400;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+      format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 100;
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Thin.woff')
+      format('woff');
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: unset;
+    border: 0;
+    font-style: normal;
+    font-family: 'Pretendard', sans-serif;
+    text-decoration-line: none;
+  }
+
+  body {
+    overflow-x: hidden;
+  }
+`;
+
+export default GlobalStyle;
---
+++ Dsm-Hackathon-2024/src/style/theme.jsx
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    blue10: "#D8DDFF",
+    blue50: "#3D54FF",
+    blue80: "#182372",
+    blue95: "#060A2C",
+    red10: "#FFD8D8",
+    red50: "#FF3D3D",
+    red80: "#721818",
+    red95: "#2C0606",
+    black0: "#FFFFFF",
+    black10: "#A6A6A6",
+    black15: "#989898",
+    black40: "#4E4E4E",
+    black50: "#303030",
+    black80: "#131313",
+  },
+};
