abe11658b5aee62b6061fd37edad948fd3b6b0e7
정승우 <ssounsowns@gmail.com>
2024-07-17 00:09:52 +0900

    로그인 제목 부분 완성
    조금 모자른 반응형 사용

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,22 +1,57 @@
 import styled from "styled-components";
 
+function Login() {
+  return (
+    <>
+      <Container>
+        <Title>로그인</Title>
+        <IdContainer>
+          <Label for="Id">아이디</Label>
+          <Input type="text" placeholder="아이디를 입력하세요" name="Id" />
+        </IdContainer>
+        <PsContainer>
+          <Label for="Ps">비밀번호</Label>
+          <Password
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+            name="Ps"
+          />
+        </PsContainer>
+      </Container>
+    </>
+  );
+}
+
+export default Login;
+
+const Title = styled.h1`
+  font-family: "Semi Bold";
+  color: #ffffff;
+  font-size: 32px;
+  padding-bottom: 20px;
+`;
+
 const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
   flex-direction: column;
-  gap: 12px;
+  gap: 20px;
   width: 100vw;
   height: 100vh;
+  background-color: #131313;
+  padding-top: 60px;
+
+  & > h1 {
+    @media screen and (max-width: 500px) {
+      margin-right: 235px;
+    }
+  }
 `;
 const IdContainer = styled.div`
   display: flex;
   flex-direction: column;
 `;
-const PsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+const PsContainer = styled(IdContainer)``;
 
 const Input = styled.input`
   /* input */
@@ -24,48 +59,30 @@ const Input = styled.input`
   /* Auto layout */
 
   padding: 10px;
-
-  width: 328px;
+  display: flex;
+  align-items: center;
+  width: 308px;
   height: 72px;
   border-radius: 8px;
   background: #303030;
   color: #d8ddff;
 
-  /* 아이디를 입력하세요. */
-
   height: 24px;
 
+  color: #ffffff;
+
+  &::placeholder {
+    color: #ffffff;
+  }
+`;
+
+const Label = styled.label`
   font-family: "Pretendard";
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
   line-height: 24px;
-  /* identical to box height, or 171% */
-
   color: #ffffff;
 `;
 
-const Label = styled.label;
-
 const Password = styled(Input)``;
-
-function Login() {
-  return (
-    <Container>
-      <IdContainer>
-        <label for="Id">아이디</label>
-        <Input type="text" placeholder="아이디를 입력하세요" name="Id" />
-      </IdContainer>
-      <PsContainer>
-        <label for="Ps">비밀번호</label>
-        <Password
-          type="password"
-          placeholder="비밀번호를 입력하세요"
-          name="Ps"
-        />
-      </PsContainer>
-    </Container>
-  );
-}
-
-export default Login;
