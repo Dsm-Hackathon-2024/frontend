2aabf5c76da8a0edc0260cf6365ac59f425ef503
wlals <jiminelp@gmail.com>
2024-07-18 01:17:55 +0900

    user 정보 가져오기 api

--- src/utils/apis/user.js
+++ src/utils/apis/user.js
@@ -7,6 +7,10 @@ export const myInfo = async () => {
   return await instance.get(`${router}/rank`);
 };
 
+export const userInfo = async () => {
+  return await instance.get(`${router}/mypage/userinfo`);
+};
+
 export const signUp = async data => {
   return await axios.post(
     `${import.meta.env.VITE_SERVER_BASE_URL}/${router}/signup`,
