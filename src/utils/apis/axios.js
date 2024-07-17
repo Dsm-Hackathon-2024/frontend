c4bed0b30ebc7e076793d8ebc969c0b8eaca9c09
dutexion <dutexion@gmail.com>
2024-07-17 10:32:24 +0900

    axios instance

---
+++ src/utils/apis/axios.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { Cookie } from "../cookie";
+
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_BASE_URL,
+  timeout: 30000,
+});
+
+instance.interceptors.request.use(
+  res => {
+    const token = Cookie.get("accessToken");
+    if (token) res.headers.Authorization = `Bearer ${token}`;
+    return res;
+  },
+  err => {
+    alert("error");
+    console.error(err);
+  }
+);
+
+instance.interceptors.response.use(
+  res => {
+    return res;
+  },
+  err => {
+    alert("error");
+    console.error(err);
+  }
+);
